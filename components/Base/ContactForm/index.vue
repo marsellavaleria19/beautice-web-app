ff3a42fe2458c634ea1edfd2ea2e688e79ab1361
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-01-17 23:18:54 +0700

    feat: add component contact form in base component

---
+++ components/Base/ContactForm/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <form>
+    <div class="mb-[38px] md:flex">
+      <BaseInput placeholder="Firstname" class="mr-[36px] md:w-1/2 mb-[38px] md:mb-0" />
+      <BaseInput placeholder="Lastname" class="md:w-1/2" />
+    </div>
+    <div class="mb-[38px]">
+      <BaseInput placeholder="Email Address" />
+    </div>
+    <div class="mb-[38px]">
+      <BaseInput placeholder="Subject Message" />
+    </div>
+    <div class="mb-[38px]">
+      <BaseInput placeholder="Email Address" />
+    </div>
+    <div class="mb-[38px]">
+      <BaseTextArea placeholder="Your inquiry here" class="h-[239px]" />
+    </div>
+    <BaseButton class="w-full lg:max-w-[248px] h-[58px]">
+      Send Message
+    </BaseButton>
+  </form>
+</template>
+
+<script>
+
+export default {
+  name: 'BaseContactForm'
+}
+</script>
